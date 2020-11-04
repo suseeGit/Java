c5f594ab79203c4378bf2ce05cd1ae2bd3d26a6a
Suseendhiran R <71625017+suseeGit@users.noreply.github.com>
2020-11-04 19:26:30 +0530

    Write a Java program that reads an positive integer and count the number of digits the number (less than ten billion) has.

---
+++ java17.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class java17 {
+        public static void main(String args[]) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Input first number : ");
+        String txt = input.nextLine();
+        System.out.println("Number of digits in the number : " + txt.length());
+    }
+}
